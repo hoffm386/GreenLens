268c762fbb79be01c6505b52d904f0e0997b4742
Erin Hoffman <hoffm386@msu.edu>
2017-04-22 16:10:35 -0700

    Super hack basic functionality
    - Grabs the 2-th product name (for some pages this is the first, for
    others it’s third, for others it doesn’t work at all)
    - Assumes that the first word of the product name is the manufacturer
    - Defaults to saying the category is electronics
    - Sends query to WeGreen API
    - Parses ratingScore out of 100 from API response (doesn’t check if
    null)
    - Appends a div to the product name DOM element with FEARFUL FACE
    EMOJI, NEUTRAL FACE EMOJI, SMILEY FACE EMOJI, or HEART EYES EMOJI based
    on the ratingScore, as well as the ratingScore

--- injector/inject.js
+++ injector/inject.js
@@ -2,14 +2,43 @@
 
 (function() {
 
-	// just place a div at top right
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected!';
-	document.body.appendChild(div);
+	function updateDisplay(xhr, productNameElement) {
+		// JSON.parse does not evaluate the attacker's scripts.
+    	var resp = JSON.parse(xhr.responseText);
+    	var ratingScore = resp["data"]["sustainabilityRatingBasics"]["ratingScore"];
+    	var emojiFace = "unset";
+    	if (ratingScore < 25) {
+    		emojiFace = "FEARFUL FACE EMOJI";
+    	} else if (25 <= ratingScore && ratingScore < 50) {
+    		emojiFace = "NEUTRAL FACE EMOJI";
+    	} else if (50 <= ratingScore && ratingScore < 75) {
+    		emojiFace = "SMILEY FACE EMOJI";
+    	} else if (75 <= ratingScore) {
+    		emojiFace = "HEART EYES EMOJI";
+    	}
 
-	alert('inserted self... giggity');
+    	var div = document.createElement('div');
+		div.textContent = emojiFace + " | " + ratingScore.toString()+" out of 100";
+		productNameElement.appendChild(div);
+	}
+
+
+	var productNameElement = document.getElementsByClassName("a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal")[2];
+
+	var productName = encodeURIComponent(productNameElement.title);
+	var manufacturer = productName.split(" ")[0];
+	var category = "Elektronik%2C%20Foto%20%26%20Computer";
+	var xhr = new XMLHttpRequest();
+	var queryString = "https://api1.wegreen.de/services/instantProductRating.json?" + 
+						"name=" + productName + 
+						"&manufacturer=" + manufacturer +
+						"&category=" + category;
+	xhr.open("GET", queryString, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+	    	updateDisplay(xhr, productNameElement);
+	  	}
+	}
+	xhr.send();	
 
 })();
